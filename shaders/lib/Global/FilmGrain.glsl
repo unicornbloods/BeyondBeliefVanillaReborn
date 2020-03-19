b88c67346a896ba66285f73e4cbb18ff176e1152
unicornbloods <unicornbloods@protonmail.com>
2020-03-19 14:24:51 -0500

    Fixed a visual bug with FilmGrain and nightEye both enabled. Started to add ssao

--- shaders/lib/Global/FilmGrain.glsl
+++ shaders/lib/Global/FilmGrain.glsl
@@ -13,8 +13,13 @@ float depth0 = texture2D(depthtex0, texcoord).x;
         if	(depth0 < 1.0) {
         float brightness = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593)); //gets brightness of screen
         float strength = FGStrength * (1.0 - brightness * 1.9); //applies brightness to strength
-        vec2 refcoord = texcoord.st + frameTimeCounter * 0.01; //makes the texcoord move over time because the offset is frameTimeCounter which changes after every frame multiplied with 0.01 to make it slower than it is by default
-        color += vec3(randFilmGrain(refcoord + 0.1), randFilmGrain(refcoord), randFilmGrain(refcoord - 0.1)) * strength; //use the moving texcoord to make the whole noise move and use offsets for the red and blue channel to make the noise colored, without the offset red, green and blue would overlap each other and the noise would be white/gray
+        
+        if (strength >= 0) {
+            vec2 refcoord = texcoord.st + frameTimeCounter * 0.01; //makes the texcoord move over time because the offset is frameTimeCounter which changes after every frame multiplied with 0.01 to make it slower than it is by default
+            color += vec3(randFilmGrain(refcoord + 0.1), randFilmGrain(refcoord), randFilmGrain(refcoord - 0.1)) * strength; //use the moving texcoord to make the whole noise move and use offsets for the red and blue channel to make the noise colored, without the offset red, green and blue would overlap each other and the noise would be white/gray
+        } else {
+            color = color;
+        }
         }
     }
 #endif
