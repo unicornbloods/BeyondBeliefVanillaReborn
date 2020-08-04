e18ee6b9beb0cabefc8d5b261ad6c9de8da91d54
UniBlood <unicornbloods@protonmail.com>
2020-08-04 17:03:19 -0500

    Changed overworld fog to fix some bugs. Disabled custom clouds by default.

--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -10,7 +10,7 @@
 			#define OFOGI 1.7 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9 5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9 6.0 10.0]
 			#define SWAMPFOG
 			#define LBOFOG //Toggle the land based only fog effect. Will affect clouds and sky with it off based on the intensity.
-			
+			#define oldFog 0 //[0 1]
 
 		// 0 = vanilla, 1 = custom
 		#define waterFog 0 //[0 1]
@@ -114,8 +114,12 @@
 							vec3 fogcolor = fogColor;
 						#endif
 					
-						color.rgb = mix(color.rgb, fogcolor, length(viewPos) / (far * OFOGI));
-						color.rgb -= near;
+						#if oldFog == 1
+							color.rgb = mix(color.rgb, fogcolor, length(viewPos) / (far * OFOGI));
+							color.rgb -= near;
+						#else
+							color.rgb = mix(color.rgb, fogcolor, min(GetDepthLinear(texcoord.st) * OFOGI / far, 0.8));
+						#endif
 					}
 			#ifdef LBOFOG
 				}
