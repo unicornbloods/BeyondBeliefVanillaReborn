af38e0cc1574740b9a762f43a5edf2f170baf360
UniBlood <unicornbloods@protonmail.com>
2020-08-30 14:30:03 -0500

    Remodeled Options menu "Stole" BSL layout since I like how clean it is

--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -145,8 +145,6 @@
 			float fogIntensity = NFOGI;
 			float fogAlpha = NFOGA;
 
-		#define NetherDarkness 0.8 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
-
 		/* #### Variables #### */
 
 			uniform sampler2D gcolor;
@@ -164,19 +162,6 @@
 
 
 		/* #### Functions #### */
-
-			void darkerShadows(inout vec3 color) {
-				float weight = 15;
-
-				color.rgb = pow(color.rgb, vec3(2.2)); // convert from gamma to linear
-
-				float brightness = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593));
-				float amount = 0.01 / (pow(brightness * weight, 2.0) + 0.01);
-
-				color = mix(color, color * NetherDarkness, amount);
-
-				color.rgb = pow(color.rgb, vec3(1.0 / 2.2)); // convert from linear to gamma
-			}
 
 			float GetDepthLinear(in vec2 coord) { //Function that retrieves the scene depth. 0 - 1, higher values meaning farther away
 				return 2.0f * near * far / (far + near - (2.0f * texture2D(gdepthtex, coord).x - 1.0f) * (far - near));
@@ -215,8 +200,6 @@
 
 				color = mix(color, fogColor, min(GetDepthLinear(texcoord.st) * 500.0 / far, 0.955));
 			}
-
-			darkerShadows(color.rgb);
 
 			/* DRAWBUFFERS:0 */
 			gl_FragData[0] = vec4(color, 1.0); //gcolor
