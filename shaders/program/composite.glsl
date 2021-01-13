2b42cbbd4767bb5089f396b56378348716e462da
UniBlood <unicornbloods@protonmail.com>
2021-01-13 13:55:10 -0600

    Added blindess

--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -38,6 +38,7 @@
 			uniform mat4 gbufferProjectionInverse;
 			uniform vec3 cameraPosition;
 			uniform mat4 gbufferModelViewInverse; 
+			uniform float blindness;
 
 			// for underwater fog
 			uniform int    isEyeInWater;
@@ -109,8 +110,10 @@
 			#endif
 					if (isEyeInWater == 0) { // Above water
 						worldPos.y /= 100;
-						float FogIntensity = clamp((mix(OFOGI, (OFOGI / 2.0), rainStrength) * worldPos.y), 0.0, 1);
-						// float FogIntensity = OFOGI;
+						// add height based and rain strength
+						float FogIntensity = mix(OFOGI, (OFOGI * 0.5), rainStrength) * worldPos.y;
+						// add blindness
+						FogIntensity = mix(FogIntensity, (FogIntensity * 0.1), blindness);
 					
 						#if OFOG == 1
 							vec3 fogcolor = clamp((fogColor * mix(fogColor, (vec3(OFOGR, OFOGG, OFOGB) / 255), 1) * worldPos.y), 0.0, 1.0);
@@ -118,7 +121,7 @@
 							vec3 fogcolor = clamp((fogColor * worldPos.y), 0.0, 1.0);
 						#endif
 					
-						color.rgb = mix(color.rgb, fogcolor, clamp(length(viewPos) / (far * FogIntensity) - near * 17, 0.0, 1.0));
+						color.rgb = mix(color.rgb, fogcolor, clamp(length(viewPos) / (far * clamp(FogIntensity, 0.0, 1.0)) - near * 17, 0.0, 1.0));
 					}
 			#ifdef LBOFOG
 				}
@@ -147,7 +150,6 @@
 			#define NFOGB 25 //[0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0 90.0 95.0 100.0 105.0 110.0 115.0 120.0 125.0 130.0 135.0 140.0 145.0 150.0 155.0 160.0 165.0 170.0 175.0 180.0 185.0 190.0 195.0 200.0 205.0 210.0 215.0 220.0 225.0 230.0 235.0 240.0 245.0 250.0 255.0]
 			#define NFOGA 123 //[0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0 90.0 95.0 100.0 105.0 110.0 115.0 120.0 125.0 130.0 135.0 140.0 145.0 150.0 155.0 160.0 165.0 170.0 175.0 180.0 185.0 190.0 195.0 200.0 205.0 210.0 215.0 220.0 225.0 230.0 235.0 240.0 245.0 250.0 255.0]
 			#define NFOGI 2.2 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8 4.9 5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9 6.0]
-			float fogIntensity = NFOGI;
 			float fogAlpha = NFOGA;
 
 		/* #### Variables #### */
@@ -161,7 +163,8 @@
 			uniform sampler2D depthtex0;
 			uniform sampler2D depthtex1;
 			uniform sampler2D gdepthtex;
-			uniform mat4 gbufferProjectionInverse; 
+			uniform mat4 gbufferProjectionInverse;
+			uniform float blindness;
 
 			uniform int isEyeInWater;
 
@@ -188,11 +191,14 @@
 
 				// end for fog
 
+				float FogIntensity = NFOGI;
+				FogIntensity = mix(FogIntensity, FogIntensity * 0.1, blindness);
+
 				vec3 fogColor = pow(vec3(NFOGR, NFOGG, NFOGB) / 255.0, vec3(2.2));
 
 				// color.rgb = mix(color.rgb, fogColor, length(viewPos) / (far * NFOGI)); // Proper but doesn't color the sky orange
 
-				color.rgb = mix(color.rgb, fogColor, min(GetDepthLinear(texcoord.st) * fogIntensity / far, (fogAlpha / 255.0)));
+				color.rgb = mix(color.rgb, fogColor, min(GetDepthLinear(texcoord.st) * clamp(FogIntensity, 0.0, 1.0) / far, (fogAlpha / 255.0)));
 
 			#endif
 
@@ -235,6 +241,7 @@
 			uniform sampler2D depthtex0;
 			uniform sampler2D depthtex1;
 			uniform sampler2D gdepthtex;
+			uniform float blindness;
 
 		/* #### Functions #### */
 
@@ -259,6 +266,7 @@
 			float depth = texture2D(depthtex1, texcoord.xy).x;
 
 				float FogIntensity = EFOGI;
+				FogIntensity = mix(FogIntensity, FogIntensity * 0.1, blindness);
 			
 				#if EFOG == 1
 					vec3 fogcolor = fogColor * mix(fogColor, (vec3(EFOGR, EFOGG, EFOGB) / 255), 0.50);
@@ -271,7 +279,7 @@
 				#endif
 
 				// color.rgb = mix(color.rgb, fogcolor, length(viewPos) / (far * EFOGI));
-				color.rgb = mix(color.rgb, fogcolor, min(GetDepthLinear(texcoord.st) * EFOGI / far, (EFOGA / 255.0)));
+				color.rgb = mix(color.rgb, fogcolor, min(GetDepthLinear(texcoord.st) * clamp(FogIntensity, 0.0, 1.0) / far, (EFOGA / 255.0)));
 
 			//lava fog
 			if(isEyeInWater == 2) {
@@ -325,7 +333,8 @@
 			uniform sampler2D gdepthtex;
 			uniform mat4 gbufferProjectionInverse;
 			uniform vec3 cameraPosition;
-			uniform mat4 gbufferModelViewInverse; 
+			uniform mat4 gbufferModelViewInverse;
+			uniform float blindness;
 
 			// for underwater fog
 			uniform int    isEyeInWater;
@@ -397,8 +406,10 @@
 			#endif
 					if (isEyeInWater == 0) { // Above water
 						worldPos.y /= 150;
-						float FogIntensity = clamp((mix(OFOGI, (OFOGI / 2.0), rainStrength) * worldPos.y), 0.0, 1);
-						// float FogIntensity = OFOGI;
+						// add height based and rain strength
+						float FogIntensity = mix(OFOGI, (OFOGI * 0.5), rainStrength) * worldPos.y;
+						// add blindness
+						FogIntensity = mix(FogIntensity, (FogIntensity * 0.1), blindness);
 					
 						#if OFOG == 1
 							vec3 fogcolor = clamp((fogColor * mix(fogColor, (vec3(OFOGR, OFOGG, OFOGB) / 255), 1) * worldPos.y), 0.0, 1.0);
@@ -406,7 +417,7 @@
 							vec3 fogcolor = clamp((fogColor * worldPos.y), 0.0, 1.0);
 						#endif
 					
-						color.rgb = mix(color.rgb, fogcolor, clamp(length(viewPos) / (far * FogIntensity) - near * 17, 0.0, 1.0));
+						color.rgb = mix(color.rgb, fogcolor, clamp(length(viewPos) / (far * clamp(FogIntensity, 0.0, 1.0)) - near * 17, 0.0, 1.0));
 					}
 			#ifdef LBOFOG
 				}
