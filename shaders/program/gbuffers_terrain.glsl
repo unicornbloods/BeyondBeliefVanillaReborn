7590fdad0ead788293c4215694849ee03991fac4
UnicornBl00d <unicornbloods@pm.me>
2023-01-01 06:02:54 -0600

    Add defines for new waving categories

--- shaders/program/gbuffers_terrain.glsl
+++ shaders/program/gbuffers_terrain.glsl
@@ -40,8 +40,14 @@
 		#define wavingFoliage
 		#define wavingSpeed 0.40//[0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75]
 
+		#define wavingGrasses
+		#define wavingDoublePlants
+		#define wavingCrops
+		#define wavingFlowers
+		#define wavingSaplings
+		#define wavingMushrooms
 		#define wavingLeaves //This option can cause performance hits in large forests.
-
+		#define wavingVines
 
 		varying vec2 lmcoord;
 		varying vec2 texcoord;
